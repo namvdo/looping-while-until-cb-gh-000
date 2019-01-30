65f64ddb00585b9a3e15bc11f8d1ec7de52cb058
Nam V. Do <namdvhe140597@fpt.edu.vn>
2019-01-30 09:59:01 +0000

    Done.

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,7 @@
 def using_until
-  levitation_force = 6
-  #your code here
-    
+  levitate_force = 6
+  until levitate_force == 10
+    puts "Wingardium Leviosa"
+    levitate_force += 1
+  end
 end
-
--- lib/while.rb
+++ lib/while.rb
@@ -1,7 +1,7 @@
 def using_while
-  levitation_force = 6
-	
-  #your code here
+  levitation = 6
+  while levitation < 10
+    levitation += 1
+    puts "Wingardium Leviosa"
+  end
 end
-
-
